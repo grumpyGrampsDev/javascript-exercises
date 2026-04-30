b9a2a7a7d18f5d37514e4a4c34e3c320c42d7751
grumpyGrampsDev <271845063+82fnszt9h9-code@users.noreply.github.com>
2026-04-30 18:25:57 -0400

    initial commit

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {};
+// Step 1: split() the string and place characters in an array
 
-};
+// Step 2: use reverse() to reverse the newly created array
+
+// Step 3: use join() to rejoin split characters
+
+// Step 4: return the reversed string
 
 // Do not edit below this line
 module.exports = reverseString;
