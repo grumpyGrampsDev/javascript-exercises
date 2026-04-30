ae1f424e70e550571fb5b123277b3576c57956ad
grumpyGrampsDev <271845063+82fnszt9h9-code@users.noreply.github.com>
2026-04-30 18:17:43 -0400

    repeat function construction

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+console.log("Script loaded");
 
+const repeatString = function (str, num) {
+  if (num < 0) return "Error";
+
+  let result = "";
+
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
