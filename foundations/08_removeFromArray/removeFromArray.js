fb52f63f51f6dce563bfd89116a93d8f7afa1c5a
grumpyGrampsDev <271845063+82fnszt9h9-code@users.noreply.github.com>
2026-04-30 21:00:13 -0400

    Add array filtering utility using rest parameters and forEach
    Implement removeFromArray function that accepts a variable number of
    values using the rest parameter syntax. Iterate through the input array
    with forEach and build a new array excluding any elements that match the
     provided arguments.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+// using a rest parameter "...theArgs" allows for expansion of arguments
+const removeFromArray = function (array, ...theArgs) {
+  const filterArray = [];
+  // forEach steps through each item of an array and executes the function
+  array.forEach((element) => {
+    // if the item is not in the array...
+    if (!theArgs.includes(element)) {
+      // push adds it to the end of the array
+      filterArray.push(element);
+    }
+  });
+  // return the new array, excluding the values passed to the
+  // removeFromArray() function
+  return filterArray;
 };
 
 // Do not edit below this line
