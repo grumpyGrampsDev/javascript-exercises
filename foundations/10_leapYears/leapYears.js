8f693a4dc84f4fd58a82c26c56a86887c5d63a40
grumpyGrampsDev <271845063+82fnszt9h9-code@users.noreply.github.com>
2026-05-03 14:50:20 -0400

    Implement leap year checker using simplified boolean logic
    Determine leap years based on divisibility rules
    Return result using single boolean expression
    Handle standard and century-year exceptions; divisible by 400

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function (num) {
+  let year = num;
+  // 1. It is divisible by 4,
+  // 2. except years divisible by 100,
+  // 3. unless they are also divisible by 400.
+  if (year % 4 !== 0) {
+    return false;
+  } else if (year % 100 !== 0) {
+    return true;
+  } else if (year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
